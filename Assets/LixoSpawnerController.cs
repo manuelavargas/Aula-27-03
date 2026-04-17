06af97e484da2de1c2ebfb42dfccb524c0300141
Usagi-Gb <2008manuelavargas@gmail.com>
2026-04-17 16:12:14 -0300

    17/04

--- Assets/LixoSpawnerController.cs
+++ Assets/LixoSpawnerController.cs
@@ -8,27 +8,33 @@ public class LixoSpawnerController : MonoBehaviour
     public float fixedY;
     public float fixedZ;
     public float timer;
+
     public GameObject Lixo;
     public int MaxPoints;
-    public int points = 0;  
-      void Start()
+    public int points = 0;
+
+    void Start()
     {
-         StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnRoutine()
     {
-        
+        while (points < MaxPoints)
+        {
+            Instantiate(Lixo,
+                new Vector3(Random.Range(-maximumX, maximumX + 1),
+                fixedY,
+                fixedZ),
+                Quaternion.identity);
+
+            yield return new WaitForSeconds(timer);
+        }
     }
-    IEnumerator SpawnRoutine() {
-    while (points < MaxPoints) {
-        Instantiate(Lixo,
-            new Vector3(Random.Range(-maximumX, maximumX + 1),
-            fixedY,
-            fixedZ),
-            Quaternion.identity);
-        yield return new WaitForSeconds(timer);
+
+    public void AddToPoints(int valor)
+    {
+        points += valor;
+        Debug.Log("Pontos: " + points);
     }
-}
 }
